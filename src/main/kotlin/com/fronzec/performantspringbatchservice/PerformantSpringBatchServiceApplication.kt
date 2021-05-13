688c65dea3a3a1c05d5f3813c24d1275bcfeea51
Eduardo Flores <efloresambrosio@gmail.com>
2021-05-12 21:45:04 -0500

    new: inital code generated by sprint initializer

---
+++ src/main/kotlin/com/fronzec/performantspringbatchservice/PerformantSpringBatchServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.fronzec.performantspringbatchservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PerformantSpringBatchServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PerformantSpringBatchServiceApplication>(*args)
+}
