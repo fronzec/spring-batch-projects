7dd23ee2bccea36f03820f79aa25c77082ab84a6
Eduardo Flores <efloresambrosio@gmail.com>
2021-05-14 02:19:15 -0500

    chg: changing our code to java code, kotlin removed

---
+++ single-threaded/src/main/java/com/fronzec/myservice/SpringBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fronzec.myservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchServiceApplication {
+
+    public static void main(String[] args) {
+        System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchServiceApplication.class, args)));
+
+    }
+}
--- single-threaded/src/main/kotlin/com/fronzec/batch/PersonRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.fronzec.batch
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PersonRepository : JpaRepository<Person, Long> {
-    fun getByAddressStreet(street: String): Person?
-}
