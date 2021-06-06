13ddcdef82efc5d4e0ec6201869d2145ec541665
Eduardo Flores <eduardo.flores@mercadolibre.com.mx>
2021-06-06 00:06:46 -0500

    new: inital code to run single jobs by same params

--- single-threaded/src/main/java/com/fronzec/myservice/web/JobController.java
+++ single-threaded/src/main/java/com/fronzec/myservice/web/JobController.java
@@ -1,10 +1,16 @@
 package com.fronzec.myservice.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fronzec.myservice.batch.JobsManagerService;
+import com.fronzec.myservice.utils.JsonUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,25 +18,36 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Este controlador expone endpoints para la gestion de los jobs
+ */
 @RestController
 @RequestMapping("/jobs")
 public class JobController {
 
     private final JobsManagerService jobsManagerService;
+    private final Logger logger = LoggerFactory.getLogger(JobController.class);
 
     public JobController(JobsManagerService jobsManagerService) {
         this.jobsManagerService = jobsManagerService;
     }
 
     @PostMapping("/start-all")
-    public ResponseEntity<JobInfo> startAllAction(
-            @Valid @RequestBody JobDataRequest dataRequest
-            ) {
-        HashMap<String, String> stringStringHashMap = jobsManagerService.launchAllJobs(dataRequest.getDate(), dataRequest.getTryNumber());
+    public ResponseEntity<JobInfo> startAllAction(@Valid @RequestBody JobDataRequest dataRequest) {
+        HashMap<String, String> stringStringHashMap = jobsManagerService.launchAllJobs(dataRequest.getDate(),
+                dataRequest.getTryNumber());
         JobInfo jobInfo = new JobInfo();
         jobInfo.setInfo(stringStringHashMap);
         return ResponseEntity.ok(jobInfo);
     }
+
+    @PostMapping(value="/start-single")
+    public ResponseEntity<String> postMethodName(@RequestHeader(value = "X-User", required = true) String user,
+                                                 @Valid @RequestBody LaunchJobRequest request) {
+        logger.info("user -> {}, reqBody -> {}", user, request);
+        return ResponseEntity.ok("{}");
+    }
+
 
     @PostMapping("/stop-all")
     public ResponseEntity<JobInfo> stopAllAction() {
