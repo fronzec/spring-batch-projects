19d5c818615bc0c39523ccd6dbcc44b013340c92
Eduardo Flores <efloresambrosio@gmail.com>
2022-09-13 01:48:45 -0500

    new(single-threaded): step 3 writer wich sends json payload Also included: - example env file - updated application.properties file - updated mocks.yaml

--- single-threaded/src/main/java/com/fronzec/myservice/batch/job1/step3/Step3Writer.java
+++ single-threaded/src/main/java/com/fronzec/myservice/batch/job1/step3/Step3Writer.java
@@ -1,9 +1,11 @@
 package com.fronzec.myservice.batch.job1.step3;
 
 import com.fronzec.myservice.batch.persons.ProcessIndicatorItemWrapper;
-import com.fronzec.myservice.utils.JsonUtils;
+import com.fronzec.myservice.restclients.ApiClient;
+import com.fronzec.myservice.restclients.BatchItemsPayload;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
@@ -14,8 +16,22 @@ public class Step3Writer implements ItemWriter<ProcessIndicatorItemWrapper<Paylo
 
   Logger logger = Logger.getLogger(Step3Writer.class.getName());
 
+  private final ApiClient apiClient;
+
+  public Step3Writer(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
   @Override
   public void write(List<? extends ProcessIndicatorItemWrapper<PayloadItemInfo>> items) {
-    logger.info("items size to send " + items.size() + JsonUtils.parseObject2Json(items));
+    List<PayloadItemInfo> payloadItemInfos = items.stream()
+            .map(ProcessIndicatorItemWrapper::getItem)
+            .collect(Collectors.toList());
+    if (apiClient.sendBatch(new BatchItemsPayload(payloadItemInfos))) {
+      logger.info("items size sent -> " + items.size());
+    } else {
+      // TODO: 13/09/2022 finalize the job manually
+      logger.info("cannot send items -> " + items.size());
+    }
   }
 }
