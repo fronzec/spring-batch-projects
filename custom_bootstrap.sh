d8287324abb25aba66180da29f5ce3cf31426dcd
Eduardo Flores <efloresambrosio@gmail.com>
2022-10-15 07:03:16 +0000

    chg(gitpod): udpate script format

--- custom_bootstrap.sh
+++ custom_bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-source ~/.bashrc
+#!/usr/bin/env bash
+source /home/gitpod/.bashrc
 /home/gitpod/.pyenv/shims/pipx install mockintosh
 /home/gitpod/.pyenv/shims/pipx inject mockintosh markupsafe==2.0.1
