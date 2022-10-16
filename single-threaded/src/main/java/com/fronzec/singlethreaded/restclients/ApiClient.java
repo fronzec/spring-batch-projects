216bc0655ebbfb9e7d8704245107c712a650b55a
Eduardo Flores <efloresambrosio@gmail.com>
2022-10-16 07:26:49 +0000

    chg(single-threaded): now info is sent to the batch endpoint - improvements on the API Client

--- single-threaded/src/main/java/com/fronzec/singlethreaded/restclients/ApiClient.java
+++ single-threaded/src/main/java/com/fronzec/singlethreaded/restclients/ApiClient.java
@@ -11,31 +11,34 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class ApiClient {
 
-  private final RestTemplate restTemplate;
+  private final RestTemplate restTemplateDispatch;
+  private final String batchServiceDispatchUrl;
 
   Logger logger = Logger.getLogger(ApiClient.class.getName());
 
   public ApiClient(RestTemplateBuilder restTemplateBuilder,
+          @Value("${single_threaded.rest_clients.client1.base_url}")
+          String batchServiceHost,
+          @Value("${single_threaded.rest_clients.client1.dispatch_path.post}")
+          String batchServiceDispatchPath,
           @Value("${single-threaded.rest-clients.client1.connection-timeout-millis}")
           int connectionTimeoutMillis,
           @Value("${single-threaded.rest-clients.client1.response-timeout-millis}")
-          int readTimeoutMillies) {
-    this.restTemplate = restTemplateBuilder.setConnectTimeout(Duration.ofMillis(connectionTimeoutMillis))
-            .setReadTimeout(Duration.ofMillis(readTimeoutMillies))
+          int readTimeoutMillis) {
+    this.batchServiceDispatchUrl = UriComponentsBuilder.fromHttpUrl(batchServiceHost + batchServiceDispatchPath).toUriString();
+    this.restTemplateDispatch = restTemplateBuilder.setConnectTimeout(Duration.ofMillis(connectionTimeoutMillis))
+            .setReadTimeout(Duration.ofMillis(readTimeoutMillis))
             .build();
   }
 
   public boolean sendBatch(BatchItemsPayload payload) {
-    HttpHeaders httpHeaders = new HttpHeaders();
-    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-    // TODO: 12/09/2022 load the URL
-    String url = "http://localhost:8081/mock-service/receive-batch";
-    HttpEntity<BatchItemsPayload> request = new HttpEntity<>(payload, httpHeaders);
-    ResponseEntity<Object> response = restTemplate.postForEntity(url, request, Object.class);
+    HttpEntity<BatchItemsPayload> request = new HttpEntity<>(payload, createDefaultHeaders());
+    ResponseEntity<Object> response = restTemplateDispatch.postForEntity(batchServiceDispatchUrl, request, Object.class);
     // TODO: 12/09/2022 handle exceptions
     if (response.getStatusCode() == HttpStatus.OK) {
       return true;
@@ -43,6 +46,13 @@ public class ApiClient {
 
     logger.warning("batch cannot be sent successfully");
     return false;
+  }
+
+  private HttpHeaders createDefaultHeaders() {
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+    httpHeaders.add("X-FronzecInc-Caller", "SingleThreadedService");
+    return httpHeaders;
   }
 
 }
