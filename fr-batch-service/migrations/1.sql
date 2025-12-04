c3c9a84c1a8dc5a75a866a040ceec1a0053cab86
Eduardo Flores <efloresambrosio@gmail.com>
2025-12-03 18:44:11 -0600

    Added: Initial migration script for MySQL batch sequence
    Introduced a new migration script to handle MySQL batch sequence changes by creating a new table `BATCH_JOB_INSTANCE_SEQ`. The script includes steps to migrate the existing values and references best practices from the Spring Batch project.

---
+++ fr-batch-service/migrations/1.sql
@@ -0,0 +1,9 @@
+
+-- MySQL: sequence emulated as a table
+-- https://github.com/spring-projects/spring-batch/blob/main/spring-batch-core/src/main/resources/org/springframework/batch/core/migration/6.0/migration-mysql.sql
+-- RENAME TABLE BATCH_JOB_SEQ TO BATCH_JOB_INSTANCE_SEQ;
+
+-- Alternative (MySQL): create new table, migrate value, drop old
+CREATE TABLE BATCH_JOB_INSTANCE_SEQ (ID BIGINT NOT NULL);
+INSERT INTO BATCH_JOB_INSTANCE_SEQ (ID) SELECT ID FROM BATCH_JOB_SEQ;
+-- DROP TABLE BATCH_JOB_SEQ;
